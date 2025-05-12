14fd0ada7c8e354b4318d703e5dde9167134f67c
Kancherla Yogitha <999627@grundfos.com>
2025-05-12 14:45:20 +0530

    intial commit

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,35 @@
+using System;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[Controller]")] // /api/users
+public class UsersController:ControllerBase
+{
+    private readonly DataContext _context;
+    public UsersController(DataContext context)
+    {
+        _context = context;
+    }
+[HttpGet]
+public async Task<ActionResult<IEnumerable<AppUser>>>GetUserS()
+{
+    var users = await _context.Users.ToListAsync();
+
+    return Ok(users);
+}
+
+[HttpGet("{id:int}")]  //api/users/3
+public async Task<ActionResult<AppUser>>GetUsers(int id)
+{
+    var user = await _context.Users.FindAsync(id);
+
+     if(user == null) return NotFound();  //404 error
+
+        return user;
+}
+}
